b804dd2715935d9e0c68d6832c89fe3c3f57418e
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-06-05 00:40:06 +0900

    Feat:Ts installation completed

--- train.ts
+++ train.ts
@@ -1,8 +1,10 @@
-const array_1: number[] = [5, 21, 12, 21, 8];
+// G-TASK: 
+// Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
+// MASALAN: getHighestIndex([5, 21, 12, 21, 8]) return qiladi 1 sonini.
 
+@MITASK
+const array_1: number[] = [5, 21, 12, 21, 8];
 const getHighestIndex_1 = (numbers: number[]) => {
   return numbers.indexOf(Math.max(...numbers));
 };
-
 const result_1 = getHighestIndex_1(array_1);
-console.log("(method one) The first max value's index is:", result_1);
