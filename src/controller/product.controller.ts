81b547d3dab9373c9faf8feb4760483f03292afd
Jasurbek Ergashaliev <braveredmi@gmail.com>
2024-07-01 22:27:26 +0900

    fix:product pages develop

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -30,12 +30,12 @@ productController.createNewProduct = async (
 ) => {
 	try {
 		console.log("createNewProduct");
-		
+    console.log("req.body:", req.body);
 		if (!req.files?.length)
 			throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
 
 		const data: ProductInput = req.body;
-		data.productImages = req.files?.map(ele => {
+		data.productImages = req.files?.map((ele) => {
       return ele.path.replace(/\\/g, "/"); 
 		});
 
--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,1 +1,83 @@
 console.log("Products frontend javascript file");
+
+
+$(function () {
+  $(".product-collection").on("change", () => {
+    const selectedValue = $(".product-collection").val();
+    if (selectedValue === "DRINK") {
+      $("#product-volume").show();
+       $("#product-collection").hide();
+    } else {
+      $("#product-volume").hide();
+			$("#product-collection").show();
+    }
+  })
+  $("#process-btn").on(".click", () => {
+    $(".dish-container").slideToggle(500);
+    $("#process-btn").css("display", "none");
+  });
+  $("#cancel-btn").on(".click", () => {
+		$(".dish-container").slideToggle(100);
+		$("#process-btn").css("display", "flex");
+  });
+  $(".new-product-status").on("change", async function (e) {
+    const id = e.target.id;
+    const productStatus = $(`#${id}.new-product-status`).val();
+    console.log("id:", id);
+    console.log("productStatus:", productStatus);
+
+    try {
+      const response = await axios.post(`/admin/product/${id}`, { productStatus: productStatus });
+      console.log("response:", response);
+      const result = response.data;
+      if (result.data) {
+        console.log("Product updated!");
+        $(".new-product-status").blur();
+      }else  alert("Product update failed!");
+    } catch (err){
+      console.log(err);
+      alert("Product update failed!");
+    }
+    })
+});
+function validateForm() {
+	const productName = $(".product-name").val();
+  const productPrice = $(".product-price").val();
+  const productLeftCount = $(".product-left-count").val();
+  const productCollection = $(".product-collection").val();
+  const productDesc = $(".product-desc").val();
+  const productStatus = $(".product-status").val();
+
+  if (
+    productName === "" ||
+    productPrice === "" ||
+    productLeftCount === "" ||
+    productCollection === "" ||
+    productDesc === "" ||
+    productStatus === "" 
+	) {
+    alert("Please insert all details!");
+    return false;
+  } else return true;
+}
+
+function previewFileHandler(input, order) {
+  const imgClassName = input.className;
+  console.log("input:", input);
+
+  const file = $(`.${imgClassName}`).get(0).files[0];
+  const fileType = file['type'];
+  const validImageType = ["image/jpg", "image/jpeg", "img/png"];
+
+  	if (!validImageType.includes(fileType)) {
+			alert("Please insert only jpeg, jpg and png!");
+    } else {
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function () {
+          $(`#image-section-${order}`).attr("src", reader.result);
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+}
